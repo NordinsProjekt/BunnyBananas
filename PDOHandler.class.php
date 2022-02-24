2a6a6c8f2f3cf8c24288d5cb72d7f7bc4ce69065
Markus Nordin <82381561+NordinsProjekt@users.noreply.github.com>
2022-02-24 14:36:23 +0100

    Add files via upload

---
+++ PDOHandler.class.php
@@ -0,0 +1,32 @@
+<?php
+abstract class PDOHandler
+{
+    private $database = "oasis";
+    private $host =  "localhost";
+    private $userName = "root";
+    private $password = "";
+    private $charset = "utf8mb4";
+
+    function __destruct()
+    {
+        
+    }
+    
+    protected function Connect()
+    {
+        try
+        {
+            $dsn = "mysql:host=" .$this->host . ";dbname=" .$this->database. ";charset=" .$this->charset;
+            $pdo = new PDO($dsn,$this->userName, $this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        } 
+        catch (\Throwable $e) 
+        {
+            echo "Connection failed: " .$e->getMessage();
+            die();
+        }
+    }
+}
+?>
