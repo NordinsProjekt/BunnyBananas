b3a232d892da3c1cccef54ba441499eb8644871b
Markus Nordin <82381561+NordinsProjekt@users.noreply.github.com>
2022-03-01 06:18:55 +0100

    Update.index
    Fixade misstag

--- Index.php
+++ Index.php
@@ -19,6 +19,12 @@ require_once('triggers.php');
 <header>
 <?php require_once('header.php');?>
 </header>
+<h1>WEBSHOP!</h1>
+    <a href="./">HOME</a>
+    <a href="./products">Products</a>
+    <a href="./orders">Orders</a>
+    <a href="./cart">Cart</a>
+    <br>
 <?php require_once('router.php');?>
 </body>
 </html>
