7d8fa8d061145ded9c29518c637f08e92a27c6d7
cubaz237 <cubaz16@gmail.com>
2022-03-01 16:45:57 +0100

    Klar med produktdelen!
    Påbörjat felhantering

--- Products.controller.php
+++ Products.controller.php
@@ -1,5 +1,7 @@
 <?php
 
+//Här ska all kontroll av data mellan browern och databasen ske
+
 require_once('Products.model.php');
 
 class ProductController
@@ -14,7 +16,7 @@ class ProductController
         return $model->getAllProducts();
     }
 
-    function listProducts($status)
+    function listProducts($status) //$status kollar om produkten finns i sortimentet genom discontinued = 0 eller 1
     {
         $model = new ProductDB();
         return $model->getProducts($status);
@@ -66,9 +68,19 @@ class ProductController
     function insertProduct($name, $category, $color, $description, $price, $balance)
     {
         $model = new ProductDB();
-        $colorID = $model->getColorID($color);
-        $categoryID = $model->getCategoryID($category);
-        $model->setNewProductDB($name, $categoryID, $colorID, $description, $price, $balance);
+
+        if ($name == "" || $category == "" || $color == "" || $price <= 0) {
+            //echo $msg = "Something went wrong!";
+            return -1;
+        }
+        else {
+            $colorID = $model->getColorID($color);
+            $categoryID = $model->getCategoryID($category);
+            $model->setNewProductDB($name, $categoryID, $colorID, $description, $price, $balance);
+        }
+
+
+
         
         //Lägg till inputhantering!
         // -Allt måste vara ifyllt?
@@ -101,28 +113,31 @@ class ProductController
     function checkUserInput($input, $arr)
     {
         $model = new ProductDB();
-        //$arr = $model->getAllColors();
         $input = $this->washInput($input);
-        $clr = "";
+        $temp = "";
         if ($input == NULL || $input == "") {
             return $msg = "Insertion failed: Invalid input!";
             return $input;
         }
         else{
             for ($i=0; $i<count($arr); $i++){ 
-                $clr = $arr[$i]['Name'];
-                if ($input == $clr) {
+                $temp = $arr[$i]['Name'];
+                if ($input == $temp) {
                     return $msg = "Insertion failed: Input already exists in DB!";
                     break;
                 }
             }
             //Check if input not already exists
-            if ($input != $clr) {
+            if ($input != $temp) {
                 return $input;
             } else{
                 return $msg = "ERROR! Something went wrong with the insertion.";
             }
         }
+    }
+
+    function checkFilledInput(){
+        
     }
 
 
--- views/products.php
+++ views/products.php
@@ -1,80 +1,9 @@
 <?php $controller = new ProductController();?>
 
+<?php //Här ska all utskrivt till browsern ligga?>
+
 <a href="products"><h2>Products</h2></a>
-<!-- <h3>All products:</h3> -->
 
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Category</th>
-            <th>Name</th>
-            <th>Color</th>
-            <th>Price</th>
-            <th>Description</th>
-            <th>Balance</th>
-            <th>Discontinued</th>
-        </tr>
-        <?php foreach ($controller->listAllProducts() as $row){?>
-        <tr>
-            <td><a href="?productID=<?php echo $row['ID']?>"><?php echo $row['ID']?></a></td>
-            <td><?php echo $row["Category"]?></td>
-            <td><?php echo $row["Name"]?></td>
-            <td><?php echo $row["Color"]?></td>
-            <td><?php echo $row["Price"]?></td>
-            <td><?php echo $row["Description"]?></td>
-            <td><?php echo $row["Balance"]?></td>
-            <td><?php echo $row["Discontinued"]?></td>
-        </tr>
-        <?php }?>
-    </table>
-
-<br>
-
-<?php
-    if (isset($_GET['productID'])){
-        foreach($controller->listProduct($_GET['productID']) as $value){
-            echo "<h3>Change product</h3>";
-            echo "<form action='#' method='post'>";
-            echo "<label for='productName'>Product name</label><br>";
-                echo "<input type='text' id='productName' name='productName' value='".$value['Name']."'><br><br>";
-
-                echo "<select name='categories' id='categories'>";
-                echo "<option value='".$value['Category']."'>".$value['Category']."</option>";
-                foreach ($controller->listAllCategories() as $row){
-                    echo "<option>".$row["Name"]."</option>"; //Hur tar jag bort dubbla värden?
-                } echo "</select>"."<br><br>";
-
-                echo "<select name='colors' id='colors'>";
-                echo "<option value='".$value['Color']."'>".$value['Color']."</option>"; //Visar ej färg
-                foreach ($controller->listAllColors() as $row){
-                    echo "<option>".$row["Name"]."</option>";
-                } echo "</select>"."<br><br>";
-
-                echo "<textarea name='description' rows='4'>".$value['Description']."</textarea><br><br>";
-
-                echo "<input type='text' id='update-price' name='update-price' value='".$value['Price']."'><br><br>";
-
-                echo "<label for='balance'><b>Quantity: </b></label>";
-                echo "<input type='number' id='balance' name='balance' value='".$value['Balance']."' min='1' max='100'><br><br>";
-
-                echo "<label for='discontinued'><b>In stock? </b></label>";
-                echo "<input type='number' id='discontinued' name='discontinued' value='".$value['Discontinued']."' min='0' max='1'>";
-                echo " 0 = Active product, 1 = Discontinued"."<br><br>";
-
-                echo "<input type='submit' name='update-product' value='Run changes'>";
-            echo "</form>";
-            echo $val = updateProductInDB($_GET['productID']);
-        }
-    }
-    
-    ?>
-    <!-- <label for="fname">Förnamn:</label><br>
-    <input type="text" id="fname" name="txtFirstname" value="<?php echo $row['Firstname']; ?>"><br> -->
-
-<br><br>
-
-
-<br>
 <details>
     <summary>View all available products</summary>
     <table>
@@ -122,16 +51,90 @@
         <?php }?>
     </table>
 </details>
+<br>
+
+<p>To alter the data for a product, click on the products ID-number and scroll down to input the changes</p>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Category</th>
+            <th>Name</th>
+            <th>Color</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Balance</th>
+            <th>Discontinued</th>
+        </tr>
+        <?php foreach ($controller->listAllProducts() as $row){?>
+        <tr>
+            <td><a href="?productID=<?php echo $row['ID']?>"><?php echo $row['ID']?></a></td>
+            <td><?php echo $row["Category"]?></td>
+            <td><?php echo $row["Name"]?></td>
+            <td><?php echo $row["Color"]?></td>
+            <td><?php echo $row["Price"]?></td>
+            <td><?php echo $row["Description"]?></td>
+            <td><?php echo $row["Balance"]?></td>
+            <td><?php echo $row["Discontinued"]?></td>
+        </tr>
+        <?php }?>
+    </table>
+
+
+<?php
+    if (isset($_GET['productID'])){
+        foreach($controller->listProduct($_GET['productID']) as $value){
+            echo "<h3>Change product</h3>";
+            echo "<form method='post' enctype='multipart/form-data'>";
+            echo "<label for='productName'><b>Product name</b></label><br>";
+                echo "<input type='text' id='productName' name='productName' value='".$value['Name']."'><br><br>";
+
+                echo "<label for='categories'><b>Category: </b></label>";
+                echo "<select name='categories' id='categories'>";
+                echo "<option value='".$value['Category']."'>".$value['Category']."</option>";
+                foreach ($controller->listAllCategories() as $row){
+                    echo "<option>".$row["Name"]."</option>"; //Hur tar jag bort dubbla värden?
+                } echo "</select>"."<br><br>";
+
+                echo "<label for='colors'><b>Color: </b></label>";
+                echo "<select name='colors' id='colors'>";
+                echo "<option value='".$value['Color']."'>".$value['Color']."</option>"; //Visar ej färg
+                foreach ($controller->listAllColors() as $row){
+                    echo "<option>".$row["Name"]."</option>";
+                } echo "</select>"."<br><br>";
+
+                echo "<label for='description'><b>Description</b></label><br>";
+                echo "<textarea name='description' rows='4'>".$value['Description']."</textarea><br><br>";
+
+                echo "<label for='description'><b>Price</b></label><br>";
+                echo "<input type='text' id='update-price' name='update-price' value='".$value['Price']."'><br><br>";
+
+                echo "<label for='balance'><b>Quantity: </b></label>";
+                echo "<input type='number' id='balance' name='balance' value='".$value['Balance']."' min='1' max='100'><br><br>";
+
+                echo "<label for='discontinued'><b>In stock? </b></label>";
+                echo "<input type='number' id='discontinued' name='discontinued' value='".$value['Discontinued']."' min='0' max='1'>";
+                echo " 0 = Active product, 1 = Discontinued"."<br><br>";
+
+                echo "<input type='hidden' name='productID' value='".$_GET['productID']."'>";
+
+                echo "<label for='uploadfile'><b>Add image: </b></label>";
+                echo "<input type='file' name='uploadfile' value='' /><br><br>";
+
+                echo "<input type='submit' name='update-product' value='Run changes'>";
+            echo "</form>";
+        }
+    }
+?>
 
 
 <h3>What do you want to do?</h3>
 
 <ul style="list-style: none">
-    <li><a href="?module=newProduct">Create new product [DONE]</a></li>
-    <li><a href="?module=newColor">Add new color [DONE]</a></li>
-    <li><a href="?module=newCategory">Add new category [DONE]</a></li>
-    <!-- <li><a href="?module=updateProduct">Change product</a></li> -->
-    <li><a href="?module=discontinueProduct">Discontinue product</a></li>
+    <li><a href="?module=newProduct">Create new product</a></li>
+    <li><a href="?module=newColor">Add new color</a></li>
+    <li><a href="?module=newCategory">Add new category</a></li>
+    <!-- <li><a href="?module=addPicture">Add picture</a></li> -->
 </ul>
 
 
@@ -159,208 +162,37 @@ if (isset($_GET['module']))
                 echo "<input type='text' id='new-price' name='new-price' placeholder='Price'><br><br>";
                 echo "<label for='balance'><b>Quantity: </b></label>";
                 echo "<input type='number' id='balance' name='balance' placeholder='0' min='1' max='100'><br><br>";
-                //Lägg till bild
                 echo "<input type='submit' name='submit-product' value='Execute'>";
             echo "</form>";
-            echo $val = addNewProductToDB();
             break;
 
         case 'newColor':
             echo "<h3>Add new color</h3>";
             echo "<form action='#' method='post'>";
-                //echo "<label for='new-color'><b>Add new color</b></label><br>";
                 echo "<input type='text' id='new-color' name='new-color' placeholder='Color'>";
                 echo "<input type='submit' name='submit-color' value='Execute'>";
             echo "</form>";
-            echo $val = addNewColorToDB();
             break;
 
         case 'newCategory':
             echo "<h3>Add new category</h3>";
             echo "<form action='#' method='post'>";
-                //echo "<label for='new-category'><b>Add new category</b></label><br>";
                 echo "<input type='text' id='new-category' name='new-category' placeholder='Category'>";
                 echo "<input type='submit' name='submit-category' value='Execute'>";
             echo "</form>";
-            echo $val = addNewCategoryToDB();
             break;
 
-        case 'discontinueProduct':
-            echo "<h3>Discountinue product</h3>";
-            //echo "<li><a href='#'>Choose category</a></li>";
-            echo "<label for='balance'><b>ID: </b></label>";
-                echo "<input type='number' id='balance' name='balance' placeholder='0' min='1' max='100'><br><br>";
-                echo "<input type='submit' name='submit-product' value='Execute'>";
-            echo "<br>";
-            //echo "Checkbox for security";
-            break;
-
+        // case 'addPicture':
+        //     echo "<form action='/action_page_binary.asp' method='post' enctype='multipart/form-data'>";
+        //     echo "<label for='uploadfile'><b>Choose image: </b></label>";
+        //     echo "<input type='file' name='uploadfile' value='' /><br><br>";
+        //     break;
        default:
            # code...
            break;
     
     }
 }
-
-function addNewProductToDB()
-{
-    $controller = new ProductController();
-    $name = "";
-    $category = "";
-    $color = "";
-    $description = "";
-    $price = "";
-    $balance = "";
-
-    if (isset($_POST['submit-product']))
-    {
-        echo "---INSIDE View---"."<br>";
-        if (isset($_POST['new-product']))
-        {
-            $name = $_POST['new-product'];
-            echo "Name: $name";
-            echo "<br>";
-        }
-        if (isset($_POST['categories']))
-        {
-            $category = $_POST['categories'];
-            echo "Category: $category";
-            echo "<br>";
-        }
-        if (isset($_POST['colors']))
-        {
-            $color = $_POST['colors'];
-            echo "Color: $color";
-            echo "<br>";
-        }
-        if (isset($_POST['description']))
-        {
-            $description = $_POST['description'];
-            echo "Desc: $description";
-            echo "<br>";
-        }
-        if (isset($_POST['new-price']))
-        {
-            $price = $_POST['new-price'];
-            echo "Price: $price";
-            echo "<br>";
-        }
-        if (isset($_POST['balance']))
-        {
-            $balance = $_POST['balance'];
-            echo "Balance: $balance";
-            echo "<br>";
-        }
-        else {
-            echo "Incorrect input!";
-        }
-        $controller->insertProduct($name, $category, $color, $description, $price, $balance);
-    }
-}
-
-
-function updateProductInDB($productID)
-{
-    $controller = new ProductController();
-    $id = $productID;
-    $name = "";
-    $category = "";
-    $color = "";
-    $description = "";
-    $price = "";
-    $balance = "";
-    $discontined = "";
-
-    if (isset($_POST['update-product']))
-    {
-        echo "---INSIDE View---"."<br>";
-        echo "ID: $id"."<br>";
-        if (isset($_POST['productName']))
-        {
-            $name = $_POST['productName'];
-            echo "Name: $name";
-            echo "<br>";
-        }
-        if (isset($_POST['categories']))
-        {
-            $category = $_POST['categories'];
-            echo "Category: $category";
-            echo "<br>";
-        }
-        if (isset($_POST['colors']))
-        {
-            $color = $_POST['colors'];
-            echo "Color: $color";
-            echo "<br>";
-        }
-        if (isset($_POST['description']))
-        {
-            $description = $_POST['description'];
-            echo "Desc: $description";
-            echo "<br>";
-        }
-        if (isset($_POST['update-price']))
-        {
-            $price = $_POST['update-price'];
-            echo "Price: $price";
-            echo "<br>";
-        }
-        if (isset($_POST['balance']))
-        {
-            $balance = $_POST['balance'];
-            echo "Balance: $balance";
-            echo "<br>";
-        }
-        if (isset($_POST['discontinued']))
-        {
-            $discontined = $_POST['discontinued'];
-            echo "Discontinued: $discontined";
-            echo "<br>";
-        }
-        else {
-            echo "Incorrect input!";
-        }
-        $controller->insertUpdatedProduct($id, $name, $category, $color, $description, $price, $balance, $discontined);
-    }
-}
-
-function addNewColorToDB()
-{
-    $controller = new ProductController();
-    $color = "";
-    if (isset($_POST['submit-color']))
-    {
-        if (isset($_POST['new-color']))
-        {
-            $color = ($_POST['new-color']);
-            $controller->insertColor($color);
-        }
-    }
-}
-
-function addNewCategoryToDB()
-{
-    $controller = new ProductController();
-    $category = "";
-    if (isset($_POST['submit-category']))
-    {
-        if (isset($_POST['new-category']))
-        {
-            $category = ($_POST['new-category']);
-            $controller->insertCategory($category);
-        }
-    }
-
-
-}
-
-    function changeProduct(){
-
-    }
-
-    function discontinueProduct(){
-
-    }
 
 ?>
 
